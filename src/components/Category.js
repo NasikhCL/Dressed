e6df6df6a382843d6c9a76236dd2f62ad3d30915
Nasikh CL <nasikcl@gmail.com>
2023-01-16 22:07:48 +0530

    minor change in the category name

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function Category() { 
     const[currentCategory, setCurrentCategory] = useState('')
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
 
     const onOpenModal = (cat) => {
         setCurrentCategory(cat)
@@ -32,7 +32,7 @@ export default function Category() {
                 <div className='h-80 w-full'>
                     <img className='h-full w-full rounded-xl' src="https://images.pexels.com/photos/1024984/pexels-photo-1024984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  alt='category'/>
                 </div>
-                <h4 className='font-bold text-xl'>Anniversary Dress</h4>
+                <h4 className='font-bold text-xl'>Anniversary outfits</h4>
             </div>
             <div  onClick={()=> onOpenModal('birthday-dress')} className='flex flex-col items-center  w-[230px] m-1 h-fit hover:cursor-pointer hover:border border-blue-900 rounded-xl hover:scale-105 transition-all duration-300 ease-in-out '>
                 <div className='h-80 w-full'>
