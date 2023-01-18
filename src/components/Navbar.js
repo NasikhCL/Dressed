5827e94a8237e9de566a1669f46c15b3a33d84a2
Nasikh CL <nasikcl@gmail.com>
2023-01-18 19:21:41 +0530

    navbar some changes for mobile devices in functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ export const Navbar = () => {
                 <li className='hidden lg:block mx-5 hover:text-cyan-700 hover:scale-105 cursor-pointer' onClick={()=> navigate('/')}>Home</li>
                 <li className='hidden lg:block mx-5 cursor-pointer hover:text-cyan-700 hover:scale-105 relative group ' onClick={handleHamb}> 
                    <a href='#category'>Category</a>
-                    <ul className='hidden z-90 group-hover:block absolute top-3 pt-5 w-[180px] left-0 group-hover:transition-all group-hover:duration-300'>
+                    <ul className='hidden z-90 group-hover:block absolute top-4 pt-5 w-[180px] left-0 group-hover:transition-all group-hover:duration-300 '>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('marrige-dress')}>Marrige Dress</li>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('anniversary-outfits')}>Anniversary Dress</li>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('birthday-dress')}>Birthday Dress</li>
@@ -99,19 +99,19 @@ export const Navbar = () => {
         </nav>
         
     
-        <ul className={isHambListVisible ?'text-blue-900 pb-3 lg:hidden' : 'text-blue-900 pb-3 hidden' }>
+        <ul className={isHambListVisible ? 'text-blue-900 pb-3 lg:hidden' : 'text-blue-900 pb-3 hidden'} onClick={handleHamb}>
             <li className='block px-3 my-3 hover:bg-gray-200 '>Home</li>
             <li className='block px-3 my-3 hover:bg-gray-200 '>Favourites</li>
             {/* <li className='block px-3 my-3 hover:bg-gray-200 '>Category</li> */}
-            <li className='block px-3 hover:bg-gray-200 relative group '> 
-                   <a href='#category'>Category</a>
-                    <ul className='hidden z-90 group-hover:block absolute top-3 pt-5 w-[180px] left-0 group-hover:transition-all group-hover:duration-300'>
+            <li className='block px-3 hover:bg-gray-200 relative group ' onClick={handleHamb}> 
+                   <a href='#category' onClick={handleHamb}>Category</a>
+                    <ul className='hidden z-90 group-hover:block absolute top-4 pt-5 w-[180px] left-7 border group-hover:transition-all group-hover:duration-300 z-10' >
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('marrige-dress')}>Marrige Dress</li>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('anniversary-outfits')}>Anniversary Dress</li>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('birthday-dress')}>Birthday Dress</li>
                         <li className='bg-blue-300 text-black hover:text-cyan-700 hover:bg-gray-200 p-2' onClick={()=> onOpenModal('college-fest-dress')}>College Fest Dress</li>
                     </ul>
-                </li>
+            </li>
             <li className='block px-3 my-3 hover:bg-gray-200 '>About Us</li>
             <li className='block px-3 my-3 hover:bg-gray-200 '>Contact Us</li>
             <div className=' lg:hidden lg:flex w-fit items-center relative mx-auto py-2 '>
