5022411f51965f30460de5282fff2751e78182f3
Nasikh CL <nasikcl@gmail.com>
2023-01-19 14:20:09 +0530

    no major change

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -6,7 +6,7 @@ export default function JoinUs() {
         <div className='w-fit mx-auto justify-center items-center flex flex-col  relative'>
             <h2 className='text-2xl font-medium  tracking-wider'>Join the Club</h2>
             <input type='text' className='bg-transparent border-b-2 placeholder-white outline-none mt-7 pb-2 text-base w-[240px] md:w-[300px]' placeholder='Enter your email'/>
-            <div className='absolute bottom-2 right-0 text-xl '><i className="fa-regular fa-envelope"></i></div>
+            <div className='absolute bottom-2 right-0 text-xl cursor-pointer'><i className="fa-regular fa-envelope"></i></div>
         </div>
     </div>
   )
