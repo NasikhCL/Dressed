843a1202b97dff7176958a1611db195a32e0d8e5
Nasikh CL <nasikcl@gmail.com>
2023-01-19 11:14:26 +0530

    added join club component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import ReactCarousel from '../components/ReactCarousel'
 import { Navbar } from '../components/Navbar'
 import Category from '../components/Category'
 import Footer from '../components/Footer'
+import JoinUs from '../components/JoinUs'
 
 
 
@@ -15,6 +16,7 @@ export default function Home() {
         <Navbar />
         <ReactCarousel />
         <Category />
+        <JoinUs />
         <Footer />
         {/* <div className='w-full h-[900px] bg-blue-900'></div> */}
       
