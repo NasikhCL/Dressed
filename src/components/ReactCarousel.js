d4c6335eb01a2ba4dbd7ac7c5ad562ab7377351c
Nasikh CL <nasikcl@gmail.com>
2023-01-19 14:23:42 +0530

    carousel height modified

--- src/components/ReactCarousel.js
+++ src/components/ReactCarousel.js
@@ -7,23 +7,23 @@ export default function ReactCarousel() {
   return (
 
       <Carousel  className='cursor-pointer -z-10 mx-auto'  autoPlay showThumbs={false} centerMode={false} infiniteLoop  showStatus={false}>
-        <div id='home' className=' bg-cyan-200 overflow-hidden'  style={{height:"520px"}}> 
+        <div id='home' className=' bg-cyan-200 overflow-hidden'  style={{height:"auto"}}> 
           <img className='zoom-in-out-box' src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt='carousel'/>
           <p className="legend">Wear it , Feel it</p>
         </div>
-        <div className='bg-cyan-200 overflow-hidden' style={{height:"520px"}}> 
+        <div className='bg-cyan-200 overflow-hidden' style={{height:"auto"}}> 
           <img className='zoom-in-out-box ' src="https://images.unsplash.com/photo-1513593771513-7b58b6c4af38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80" alt='carousel' />
           <p className="legend">Run for your dreams </p>
         </div>
-        <div className='bg-cyan-200 overflow-hidden' style={{height:"520px"}}> 
+        <div className='bg-cyan-200 overflow-hidden' style={{height:"auto"}}> 
           <img className=' zoom-in-out-box' src="https://images.unsplash.com/photo-1529397938791-2aba4681454f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt='carousel' />
           <p className="legend">Be simple</p>
         </div>
-        <div className='bg-cyan-200 overflow-hidden' style={{height:"520px"}}> 
+        <div className='bg-cyan-200 overflow-hidden' style={{height:"auto"}}> 
           <img className='zoom-in-out-box ' src="https://images.unsplash.com/photo-1604958567041-fc955468b7ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt='carousel' />
           <p className="legend">Watch Carefully</p>
         </div>
-        <div className='bg-cyan-200 overflow-hidden' style={{height:"520px"}}> 
+        <div className='bg-cyan-200 overflow-hidden' style={{height:"auto"}}> 
           <img className='zoom-in-out-box ' src="https://images.unsplash.com/photo-1505535162959-9bbcb4ab22d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1718&q=80" alt='carousel' />
           <p className="legend">Blind Eyes</p>
         </div>
